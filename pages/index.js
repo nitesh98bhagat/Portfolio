d541356fa3ab99bfbb393561b0cc923f376fb68b
Nitesh Bhagat <nitshbhgt1998@gmail.com>
2022-12-20 19:45:28 +0530

    Education Section added

--- pages/index.js
+++ pages/index.js
@@ -30,14 +30,16 @@ export default function HomePage() {
 
   return (
     <div
-      className=" flex flex-col bg-repeat "
       style={{
         backgroundImage: `url(https://media.istockphoto.com/id/1224388366/vector/colorful-geometric-background.jpg?s=612x612&w=0&k=20&c=0MH3nZPyJnqL41pnXHnejHlUqFBPmVVKt8dCl9tbaOs=)`,
       }}
     >
-      <div className="flex flex-col  items-center ">
+      <div
+        className="flex flex-col  items-center "
+        style={{ minHeight: "100vh" }}
+      >
         {/* Profle Bar */}
-        <div className="flex flex-row w-full sm:w-2/3  space-x-2 p-3 justify-center bg-stone-50">
+        <div className="flex flex-row w-full sm:w-2/3  space-x-2 md:space-x-10 p-3 justify-start bg-stone-50">
           {/* image */}
           <div className=" w-20 h-20 sm:w-44 sm:h-44 relative rounded-full">
             <Image
@@ -49,7 +51,7 @@ export default function HomePage() {
             />
           </div>
           {/* details */}
-          <div className="flex flex-col w-64 md:w-72 sm:w-96 ">
+          <div className="flex flex-col w-64 md:w-1/2  sm:w-96 ">
             <h1 className="text-lg sm:text-2xl font-bold">Nitesh Bhagat</h1>
             <h1 className="text-sm sm:text-base font-base text-slate-500">
               React, Next, Flutter
@@ -68,7 +70,7 @@ export default function HomePage() {
         </div>
 
         {/* Tab bar */}
-        <div className="flex flex-row justify-start  sticky top-0 items-end w-full overflow-x-auto sm:w-2/3  bg-stone-50  ">
+        <div className="flex flex-row justify-start  sticky top-0 items-end w-full overflow-hidden hover:overflow-x-auto sm:w-2/3  bg-stone-50  ">
           {["Project", "Skills", "Education", "Technologies", "About"].map(
             (e, i) => (
               <p
@@ -98,7 +100,7 @@ export default function HomePage() {
         </div>
 
         {/* Tab Bar View */}
-        <div className="flex flex-col w-full  sm:w-2/3 bg-stone-50 ">
+        <div className="flex flex-col w-full  sm:w-2/3 bg-stone-50  ">
           {tabBarView[currentIndex]}
         </div>
       </div>
@@ -108,42 +110,42 @@ export default function HomePage() {
 
 function ProjectPage() {
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-4  gap-4  text-teal-500  py-5">
-      <ProjectTiile
+    <div className="grid grid-cols-3 sm:grid-cols-4  gap-2  text-teal-500  py-5">
+      <ProjectTile
         title={"VSCODE Clone"}
         link="/vscode"
         icon={<SiVisualstudiocode className="text-6xl  text-sky-400" />}
       />
-      <ProjectTiile
+      <ProjectTile
         title={"YouTube Clone"}
         link="/vscode"
         icon={<BsYoutube className="text-6xl text-red-600 " />}
       />
-      <ProjectTiile
+      <ProjectTile
         title={"Medium Clone"}
         link="/blog"
         icon={<AiFillMediumSquare className="text-6xl text-black " />}
       />
-      <ProjectTiile
+      <ProjectTile
         title={"E-commerce"}
         link="/store"
         icon={<MdStore className="text-6xl text-gray-400" />}
       />
-      <ProjectTiile
+      <ProjectTile
         title={"Twitter clone"}
         link="/store"
         icon={<BsTwitter className="text-6xl text-sky-400 " />}
       />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
-      <ProjectTiile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
+      <ProjectTile link={"/testing"} />
 
       {/* BsTwitter */}
     </div>
@@ -151,51 +153,82 @@ function ProjectPage() {
 }
 
 function EducationPage() {
+  const educationList = [
+    {
+      degree: "Secondary Education",
+      year: "2004-2014",
+      institute: "Vidya Vikash High School Garifa",
+      subtitle: "",
+    },
+    {
+      degree: "Higher Secondary Education",
+      year: "2014-2016",
+      institute: "Dum Dum K.L.S Hindi Vidyalaya",
+      subtitle: "Science",
+    },
+    {
+      degree: "B.Sc",
+      year: "2016-2017",
+      institute: "Serampore College",
+      subtitle: "Dropped out",
+    },
+    {
+      degree: "Diploma in Coumputer Science",
+      year: "2017-2020",
+      institute: "Regent Institute Science & Technology",
+      subtitle: "",
+    },
+    {
+      degree: "B.Tech in Computer Science",
+      year: "2020-2023",
+      institute: "Regent Education and Research Foundation",
+      subtitle: "",
+    },
+  ];
+
   return (
-    <div className="flex flex-col w-full">
-      <EducationTle
-        year={"2004-2014"}
-        school="Vidya Vikash High School Garifa"
-        degree={"Secondary Education"}
-      />
-      <EducationTle
-        year={"2004-2014"}
-        school="Vidya Vikash High School Garifa"
-        degree={"Secondary Education"}
-      />
-      <EducationTle
-        year={"2004-2014"}
-        school="Vidya Vikash High School Garifa"
-        degree={"Secondary Education"}
-      />
+    <div className="flex flex-col">
+      {educationList
+        .map((e) => (
+          <EducationTile
+            key={e.degree}
+            year={e.year}
+            school={e.institute}
+            degree={e.degree}
+            subtitle={e.subtitle}
+          />
+        ))
+        .reverse()}
     </div>
   );
 }
 
-function EducationTle({ school, year, degree, subtitle }) {
+function EducationTile({ school, year, degree, subtitle }) {
   return (
-    <div className="flex bg-stone-50 p-5 space-x-8 border-b border-slate-200">
-      <div className="flex flex-col ">
-        <span class="font-semibold title-font text-gray-700">YEAR</span>
+    <div className="flex flex-row justify-start space-x-5 sm:space-x-16 bg-stone-50 p-5  border-b border-slate-200">
+      <div className="flex flex-col  ">
+        <span class="font-semibold title-font text-gray-700 text-sm ">
+          YEAR
+        </span>
         <span class="mt-1 text-gray-500 text-sm">{year ?? "2004-2014"}</span>
       </div>
       <div className="flex flex-col ">
-        <h2 class="text-xl font-medium text-gray-900 title-font w-[70vh]">
+        <h2 class="text-base sm:text-xl font-medium text-gray-900">
           {degree ?? "Lorem ipsum dolor sit amet."}
         </h2>
-        <span class="font-semibold title-font text-gray-700">
+        <span class="font-normal text-sm text-gray-500">
           {school ?? "YEAR"}
         </span>
-        <p className="text-base w-[70vh] ">{subtitle}</p>
+        <p className="text-sm ">{subtitle}</p>
       </div>
     </div>
   );
 }
 
-function ProjectTiile({ title, icon, link }) {
+function ProjectTile({ title, icon, link }) {
   return (
     <Link href={link}>
-      <div className="flex flex-col   items-center cursor-pointer  hover:bg-white  p-2 sm:p-5 shadow-md rounded-md">
+      <div className="flex flex-col   items-center cursor-pointer bg-stone-50 hover:bg-white  p-2 sm:p-5 shadow-sm hover:shadow-md rounded-md">
         {icon ?? <BsFillImageFill className="text-6xl text-gray-400" />}
         <p className="p-2">{title ?? "Vs Code Clone"}</p>
       </div>
